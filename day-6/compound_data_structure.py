dca8c09d5659addfa20e4013e418e9e579943033
ashish8796 <ashishsaini.ak@gmail.com>
2019-07-16 22:37:10 +0530

    compound data struture updated

--- day-6/compound_data_structure.py
+++ day-6/compound_data_structure.py
@@ -12,3 +12,16 @@ helium = elements["helium"]                       # get the helium dictionary
 hydrogen_weight = elements["hydrogen"]["weight"]  # get hydrogen's weight
 print(helium)
 #{'symbol': 'He', 'number': 2, 'weight': 4.002602}
+
+oxygen = {"number":8,"weight":15.999,"symbol":"O"}  # create a new oxygen dictionary 
+elements["oxygen"] = oxygen  # assign 'oxygen' as a key to the elements dictionary
+print('elements = ', elements)
+#elements =  {"hydrogen": {"number": 1,
+                          "weight": 1.00794,
+                          "symbol": 'H'},
+               "helium": {"number": 2,
+                          "weight": 4.002602,
+                          "symbol": "He"}, 
+               "oxygen": {"number": 8, 
+                          "weight": 15.999, 
+                          "symbol": "O"}}

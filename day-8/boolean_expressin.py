7140b6659f1317bb2062345fd3780fbcb1e0c300
ashish8796 <ashishsaini.ak@gmail.com>
2019-07-19 22:36:37 +0530

    Complex boolean expression

---
+++ day-8/boolean_expressin.py
@@ -0,0 +1,11 @@
+#Complex Boolean Expressions
+#'If' statements sometimes use more complicated boolean expressions for their conditions.
+
+if 18.5 <= weight / height**2 < 25:
+    print("BMI is considered 'normal'")
+
+if is_raining and is_sunny:
+    print("Is there a rainbow?")
+
+if (not unsubscribed) and (location == "USA" or location == "CAN"):
+    print("send email")

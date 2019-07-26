b36795d33c0a237ccd517a53cfc0c08fca20a796
ashish8796 <ashishsaini.ak@gmail.com>
2019-07-26 22:23:50 +0530

    continue updated

--- day-13/break_continue.py
+++ day-13/break_continue.py
@@ -16,3 +16,24 @@ for cargo_name, cargo_weight in manifest:
 
 print("\nFinal Weight: {}".format(weight))
 print("Final Items: {}".format(items))
+
+# skips an iteration when adding an item would exceed the limit
+# breaks the loop if weight is exactly the value of the limit
+print("\nMETHOD 2")
+weight = 0
+items = []
+for cargo_name, cargo_weight in manifest:
+    print("current weight: {}".format(weight))
+    if weight >= 100:
+        print("  breaking from the loop now!")
+        break
+    elif weight + cargo_weight > 100:
+        print("  skipping {} ({})".format(cargo_name, cargo_weight))
+        continue
+    else:
+        print("  adding {} ({})".format(cargo_name, cargo_weight))
+        items.append(cargo_name)
+        weight += cargo_weight
+
+print("\nFinal Weight: {}".format(weight))
+print("Final Items: {}".format(items))

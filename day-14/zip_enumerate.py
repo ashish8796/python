942a8147fc1f215ae8e088de9077eb62d9f3cdc7
ashish8796 <ashishsaini.ak@gmail.com>
2019-07-31 13:47:15 +0530

    zip added

---
+++ day-14/zip_enumerate.py
@@ -0,0 +1,12 @@
+#Zip
+#zip returns an iterator that combines multiple iterables into one sequence of tuples.
+letters = ['a', 'b', 'c']
+nums = [1, 2, 3]
+
+for letter, num in zip(letters, nums):
+    print("{}: {}".format(letter, num))
+
+#you can also unzip a list into tuples using an asterisk
+some_list = [('a', 1), ('b', 2), ('c', 3)]
+letters, nums = zip(*some_list)
+print(letters, nums)

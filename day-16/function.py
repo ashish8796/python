d3bc44c6bcd52aeb3a050aa90542fb625fc34cd2
ashish8796 <ashishsaini.ak@gmail.com>
2019-08-02 15:01:54 +0530

    Defining function added;

---
+++ day-16/function.py
@@ -0,0 +1,9 @@
+#Functions
+#Example of a function definition:
+def cylinder_volume(height, radius):
+    pi = 3.14159
+    return height * pi * radius ** 2
+
+#After defining the cylinder_volume function, we can call the function like this.
+print(cylinder_volume(10, 3)
+#output 282.7431

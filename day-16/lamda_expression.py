f9006f1217e61eccafb8a5659c3b70e352b6bf39
ashish8796 <ashishsaini.ak@gmail.com>
2019-08-03 02:45:30 +0530

    Lambda with filter built-in function

--- day-16/lamda_expression.py
+++ day-16/lamda_expression.py
@@ -3,6 +3,7 @@ multiply = lambda x, y: x * y
 print(multiply(4, 7))
 #output 28
 
+#Lambda with map() built-in function
 '''map() is a higher-order built-in function that takes a function and iterable as inputs,
  and returns an iterator that applies the function to each element of the iterable.
  '''
@@ -22,3 +23,21 @@ print(averages)
 #OR 
 averages = list(map(lambda x: sum(x)/len(x),numbers))
 print(averages)
+
+#Lambda with Filter
+"""
+filter() is a higher-order built-in function that takes a function 
+and iterable as inputs and returns an iterator with the elements 
+from the iterable for which the function returns True. 
+"""
+cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+
+def is_short(name):
+    return len(name) < 10
+
+short_cities = list(filter(is_short, cities))
+print(short_cities)
+#output ['Chicago', 'Denver', 'Boston']
+#OR
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)

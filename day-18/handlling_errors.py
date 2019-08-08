0b78384b76db2999b32c6823b341deabc0f0b2f5
ashish8796 <ashishsaini.ak@gmail.com>
2019-08-08 10:33:21 +0530

    handllin errors added

---
+++ day-18/handlling_errors.py
@@ -0,0 +1,20 @@
+#Errors And Exceptions
+""""
+Syntax errors-'''occur when Python can’t interpret our code,
+didn’t follow the correct syntax for Python'''
+Exceptions-'''occur when unexpected things happen during execution of a program,
+even if the code is syntactically correct. 
+For exmaple:-
+ValueError
+AssertionError
+IndexError
+KeyError
+TypeError'''
+"""
+
+#Try Statement:- use try statements to handle exceptions
+#try:- only mandatory clause in a try statement,
+#except:- If Python runs into an exception while running the try block,jump to the except block 
+#else:- '''Python runs into no exceptions while running the try,
+#it will run the code in this block after running the try block.'''
+#finally:- Before Python leaves this try statement, will run the code in this finally

832e12a8c7963108d587ebe018a4105145d16e59
ashish8796 <ashishsaini.ak@gmail.com>
2019-08-20 20:18:05 +0530

    password_generator and word.txt files added

--- file.py
+++ file.py
@@ -1,4 +1,13 @@
-letters = ['a', 'b', 'c', 'd', 'e']
-for i, letter in enumerate(letters):
-    print(i, letter)
-             
+user_list = []  
+list_sum = 0
+for i in range(10):
+    userInput = int(input("Enter any number: "))
+    try:
+        user_list.append(userInput)
+        if userInput % 2 == 0:
+            list_sum += userInput
+    except ValueError:
+        print("Incorrect value. That's not an int!")
+
+print("user_list: {}".format(user_list))
+print("The sum of the even numbers in user_list is: {}.".format(list_sum))

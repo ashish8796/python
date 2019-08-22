07422fa325c283e3113b3f29053af61fda3856de
ashish8796 <ashishsaini.ak@gmail.com>
2019-08-22 23:42:08 +0530

    third party libraries updated

---
+++ day-22/third_party_libraries.py
@@ -0,0 +1,32 @@
+#You can install them using pip, a package manager that is included with Python 3.
+# One popular alternative is Anaconda which is designed specifically for data science.
+#To install a package using pip, just enter "pip install" followed by the name of the package in your command line like this: 4
+pip install package_name.
+
+#Using a requirements.txt File
+#programmers often list a project's dependencies in a file called requirements.txt. 
+beautifulsoup4==4.5.1
+bs4==0.0.1
+pytz==2016.7
+requests==2.11.1
+
+#The version number is optional, but it usually should be included
+#You can use pip to install all of a project's dependencies at once by typing 
+pip install -r requirements.txt #in your command line.
+
+#Useful Third-Party Packages
+IPython - A better interactive Python interpreter
+requests - Provides easy to use methods to make web requests. Useful for accessing web APIs.
+Flask - a lightweight framework for making web applications and APIs.
+Django - A more featureful framework for making web applications. Django is particularly good for designing complex, content heavy, web applications.
+Beautiful Soup - Used to parse HTML and extract information from it. Great for web scraping.
+pytest - extends Python's builtin assertions and unittest module.
+PyYAML - For reading and writing YAML files.
+NumPy - The fundamental package for scientific computing with Python. It contains among other things a powerful N-dimensional array object and useful linear algebra capabilities.
+pandas - A library containing high-performance, data structures and data analysis tools. In particular, pandas provides dataframes!
+matplotlib - a 2D plotting library which produces publication quality figures in a variety of hardcopy formats and interactive environments.
+ggplot - Another 2D plotting library, based on R's ggplot2 library.
+Pillow - The Python Imaging Library adds image processing capabilities to your Python interpreter.
+pyglet - A cross-platform application framework intended for game development.
+Pygame - A set of Python modules designed for writing games.
+pytz - World Timezone Definitions for Python

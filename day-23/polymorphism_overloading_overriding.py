958c363fab848b4996d974c52056691a1b1bd28f
ashish8796 <ashishsaini.ak@gmail.com>
2019-09-08 19:38:53 +0530

    method overloading and method overriding added

---
+++ day-23/polymorphism_overloading_overriding.py
@@ -0,0 +1,37 @@
+# METHOD OVERLOADING
+class Stuent:
+    def __init__(self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def sum(self, a=None, b=None, c=None):
+        s = 0
+        if a != None and b != None and c != None:
+            s = a + b + c
+        elif a != None and b != None:
+            s = a+b
+        else:
+            s = a
+        return s
+
+
+s1 = Stuent(73, 72)
+
+print(s1.sum(7,8,3))
+
+# METHOD OVERRIDING
+class A:
+
+    def show(self):
+        print("In A show")
+
+
+class B(A):
+
+    def show(self):
+        print("In B show")
+
+
+a1 = B()
+
+a1.show()

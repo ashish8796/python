004a4372a5bbb0c0ddb4b397e2330a6d0425b57d
ashish8796 <ashishsaini.ak@gmail.com>
2019-09-14 01:22:35 +0530

    class method added

---
+++ day-26/class_method.py
@@ -0,0 +1,19 @@
+# Class method - they are called by a class, which is passed to the cls parameter of the method.
+# class method are marked with a classmethod decorator.
+
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def calculate_area(self):
+        return self.width * self.height
+
+    @classmethod
+    def new_square(cls, side_lenght):
+        return cls(side_lenght, side_lenght)
+
+
+square = Rectangle.new_square(5)
+print(square.calculate_area())

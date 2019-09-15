5df8f844959f023ea4ca73be03bc6ce5be2fdcbb
ashish8796 <ashishsaini.ak@gmail.com>
2019-09-15 15:09:43 +0530

    property decorator added

---
+++ day-26/property.py
@@ -0,0 +1,29 @@
+# properties are created by putting the `porperty` decorator above a method,
+# which means when the instance attribute with the same name as the method is accessed, the method will be called instead.
+# use of setter/getter
+
+
+class Pizza:
+    def __init__(self, toppings):
+        self.toppings = toppings
+        self._pineapple_allowed = False
+
+    @property
+    def pineapple_allowed(self):
+        return self._pineapple_allowed
+
+    @pineapple_allowed.setter
+    def pineapple_allowed(self, value):
+        if value:
+            passwo3r = input("Enter the password: ")
+            if password == "Sw0rdf1sh!":
+                self.pineapple_allowed = value
+            else:
+                raise ValueError("Alert! Intruder!")
+
+
+pizza = Pizza(["chesse", "tomato"])
+print(pizza.pineapple_allowed)
+pizza.pineapple_allowed = True
+
+print(pizza.pineapple_allowed)

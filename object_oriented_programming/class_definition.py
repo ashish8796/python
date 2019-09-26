d37519627d015ad5bfadc281733dbea07fd64e4f
ashish8796 <ashishsaini.ak@gmail.com>
2019-09-27 00:57:56 +0530

    integer to roman class object added

---
+++ object_oriented_programming/class_definition.py
@@ -0,0 +1,3 @@
+'''
+When a class definition is entered, a new namespace is created, 
+and used as the local scope — thus, all assignments to local variables go into this new namespace.
---
+++ object_oriented_programming/num_convert_roman.py
@@ -0,0 +1,19 @@
+class Roman:
+    def __init__(self, num):
+        self.num = num
+
+    def num_to_roman(self):
+        d = {1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L", 40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"}
+        roman_str = ""
+        while self.num > 0:
+            def check():
+                for j in d.keys():
+                    if self.num >= j:
+                        return j
+            if self.num >= check():
+                roman_str += d[check()]
+                self.num -= check()
+        return roman_str
+
+roman = Roman(735)
+print(roman.num_to_roman())

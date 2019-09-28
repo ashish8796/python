2d98384a8eb75148c18f950cba3325f1d3b01dee
ashish8796 <ashishsaini.ak@gmail.com>
2019-09-29 03:19:47 +0530

    changing color of graph and marker and using marker

---
+++ introduction_to_data_science/colors_and_markers.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt 
+
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.972]
+
+#changing the color and using marker in the graph
+plt.plot(year, pop, label='population in year', color="red", marker='o',
+markerfacecolor="g")
+
+plt.xlabel('year')
+plt.ylabel('population')
+plt.title('population per year')
+plt.legend(loc="lower right")
+plt.show()

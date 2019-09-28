88e860e98941c0c73fa5dfb179a0c0ead4c460ff
ashish8796 <ashishsaini.ak@gmail.com>
2019-09-29 03:27:28 +0530

    linestyle and linewidth added

--- introduction_to_data_science/colors_and_markers.py
+++ introduction_to_data_science/colors_and_markers.py
@@ -4,8 +4,8 @@ year = [1950, 1970, 1990, 2010]
 pop = [2.519, 3.692, 5.263, 6.972]
 
 #changing the color and using marker in the graph
-plt.plot(year, pop, label='population in year', color="red", marker='o',
-markerfacecolor="g")
+plt.plot(year, pop, label='population in year',
+color="red", marker='o', markerfacecolor="g")
 
 plt.xlabel('year')
 plt.ylabel('population')
